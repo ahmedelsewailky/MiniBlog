7ae1d793a4ab134f3b598046b670599bdf256aba
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-26 14:14:40 +0200

    Complete post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'image',
+        'title',
+        'slug',
+        'article',
+        'status',
+        'category_id',
+        'user_id',
+        'views',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
