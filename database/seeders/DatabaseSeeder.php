ebfdb7210657b7053c08abadf09455f60db5f06f
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-26 14:18:31 +0200

    Add factories and seeders at default seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(30)->create();
+        \App\Models\Post::factory(30)->create();
+
+
+        $this->call([
+            UserTableSeeder::class,
+        ]);
     }
 }
