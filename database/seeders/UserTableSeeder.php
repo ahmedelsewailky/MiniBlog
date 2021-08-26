be08be927c09e944a58e7714950e9f40ef5195ae
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-26 15:41:14 +0200

    Add image column to users table, model, seeder

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -17,27 +17,32 @@ class UserTableSeeder extends Seeder
         User::create([
             'name' => 'ahmed',
             'email' => 'ahmed@yahoo.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'image' => '/website/images/users/seeding/user-1.jpg'
         ]);
         User::create([
             'name' => 'sayed',
             'email' => 'sayed@yahoo.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'image' => '/website/images/users/seeding/user-6.jpg'
         ]);
         User::create([
             'name' => 'mostafa',
             'email' => 'mostafa@yahoo.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'image' => '/website/images/users/seeding/user-2.jpg'
         ]);
         User::create([
             'name' => 'osama',
             'email' => 'osama@yahoo.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'image' => '/website/images/users/seeding/user-5.jpg'
         ]);
         User::create([
             'name' => 'eman',
             'email' => 'eman@yahoo.com',
-            'password' => bcrypt('123456')
+            'password' => bcrypt('123456'),
+            'image' => '/website/images/users/seeding/user-4.jpg'
         ]);
     }
 }
