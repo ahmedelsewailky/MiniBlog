0a8c75333a6eb4049c0690df22c912f2eee31759
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-29 13:40:30 +0200

    Create dashboard routs file

---
+++ routes/dashboard.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebsiteController;
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register dashboard routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "dashboard" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::group([
+    'prefix' => 'panel',
+    'namespace' => 'App\Http\Controllers'
+], function(){
+
+    // Dashboard
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+});
+
