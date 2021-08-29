96d00ca28e98e054b240e788d9bc4dafac5c5b19
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-29 13:39:45 +0200

    Added site_visitors counter to table.setting

--- database/seeders/SettingTableSeeder.php
+++ database/seeders/SettingTableSeeder.php
@@ -28,6 +28,7 @@ class SettingTableSeeder extends Seeder
                 'instagram' => 'https://instagram.com/ahmedelsewailky'
             ],
             'site_description' => 'Molestias cumque aperiam qui non eum eligendi. Reprehenderit voluptatum sed architecto quo sint a. Molestias occaecati iste sit dolores dolor aliquid. Minus aut voluptas sed delectus dolorem quam soluta. Sint veritatis ab sit ducimus illum. At corporis delectus consequuntur qui magnam tempora. Qui maxime porro ex rerum ea deleniti praesentium nobis. Ea possimus necessitatibus sit aliquid optio laborum at. Non laborum illum necessitatibus doloribus incidunt enim temporibus. Adipisci sed enim laudantium sint tempore. Voluptas voluptatem aut debitis rem.',
+            'site_visitors' => '0'
         ]);
     }
 }
