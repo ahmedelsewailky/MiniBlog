3022cf3b13624cab3e2096243663a4f59423cf72
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-29 12:25:46 +0200

    Repaire html template structure with imporving

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -1,171 +1,119 @@
- <!DOCTYPE html>
- <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
- <head>
-     <title>{{ config('app.name') }} - @yield('title', 'Undefibed')</title>
-     <meta charset="utf-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <title>{{ config('app.name') }} - @yield('title', 'Undefibed')</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-     <link href="../../css.css?family=Muli:300,400,700|Playfair+Display:400,700,900" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{ asset('website') }}/css/bootstrap.min.css">
+    <!-- FontAwesome CDN -->
+    <link rel="stylesheet" href="{{ asset('website') }}/fonts/fontawesome/css/all.min.css">
+    <!-- Custome Style File -->
+    <link rel="stylesheet" href="{{ asset('website') }}/css/style.css">
+</head>
 
-     <link rel="stylesheet" href="{{ asset('website') }}/fonts/icomoon/style.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/bootstrap.min.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/magnific-popup.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/jquery-ui.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/owl.carousel.min.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/owl.theme.default.min.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/bootstrap-datepicker.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/fonts/flaticon/font/flaticon.css">
-     <link rel="stylesheet" href="{{ asset('website') }}/css/aos.css">
-     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
+<body>
 
-     <link rel="stylesheet" href="{{ asset('website') }}/css/style.css">
- </head>
-
- <body>
-
-     <div class="site-wrap">
-         <div class="site-mobile-menu">
-             <div class="site-mobile-menu-header">
-                 <div class="site-mobile-menu-close mt-3">
-                     <span class="icon-close2 js-menu-toggle"></span>
-                 </div>
-             </div>
-             <div class="site-mobile-menu-body"></div>
-         </div>
-
-
-         <!-- Navbar -->
-         <header class="site-navbar" role="banner">
-             <div class="container">
-                 <div class="row align-items-center">
-
-                     <div class="col-12 search-form-wrap js-search-form">
-                         <form method="get" action="#">
-                             <input type="text" id="s" class="form-control" placeholder="Search...">
-                             <button class="search-btn" type="submit"><span class="icon-search"></span></button>
-                         </form>
-                     </div>
-
-                     <div class="col-4 site-logo">
-                         <a href="{{ route('website') }}" class="nav-link">Mini Blog</a>
-                     </div>
-
-                     <div class="col-8 text-end">
-                         <nav class="site-navigation" role="navigation">
-                             <ul class="site-menu js-clone-nav ms-auto d-none d-lg-block mb-0">
-                                 @foreach ($categories as $category)
-                                 <li><a
-                                         href="{{ url('/category', $category->slug) }}">{{ Str::ucfirst($category->name) }}</a>
-                                 </li>
-                                 @endforeach
-                                 <li class="d-none d-lg-inline-block"><a href="#" class="js-search-toggle"><span
-                                             class="icon-search"></span></a></li>
-                             </ul>
-                         </nav>
-                         <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span
-                                 class="icon-menu h3"></span></a>
-                     </div>
-                 </div>
-
-             </div>
-         </header>
+    <div class="site-wrap">
+        <nav class="navbar site-navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="{{ route('website') }}"><span class="text-orange">Mini</span>Blog</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        @foreach ($categories as $category)
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="{{ url('/category', $category->slug) }}">{{ Str::ucfirst($category->name) }}</a>
+                        </li>
+                        @endforeach
+                    </ul>
+                    <form class="d-flex nav-search-form">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn" type="submit"><i class="fas fa-search"></i></button>
+                    </form>
+                </div>
+            </div>
+        </nav>
 
 
-         @yield('content')
+        @yield('content')
 
 
-         <!-- NewsLetter -->
-         <div class="site-section bg-lightx">
-             <div class="container">
-                 <div class="row justify-content-center text-center">
-                     <div class="col-md-5">
-                         <div class="subscribe-1 ">
-                             <h2>Subscribe to our newsletter</h2>
-                             <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt
-                                 error illum a explicabo, ipsam nostrum.</p>
-                             <form action="#" class="d-flex">
-                                 <input type="text" class="form-control" placeholder="Enter your email address">
-                                 <input type="submit" class="btn btn-primary" value="Subscribe">
-                             </form>
-                         </div>
-                     </div>
-                 </div>
-             </div>
-         </div>
+        <!-- NewsLetter -->
+        <div class="site-section bg-lightx">
+            <div class="container">
+                <div class="row justify-content-center text-center">
+                    <div class="col-md-5">
+                        <div class="subscribe-1 ">
+                            <h2>Subscribe to our newsletter</h2>
+                            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit nesciunt
+                                error illum a explicabo, ipsam nostrum.</p>
+                            <form action="#" class="d-flex">
+                                <input type="text" class="form-control" placeholder="Enter your email address">
+                                <input type="submit" class="btn btn-primary" value="Subscribe">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
-         <!-- About Us -->
-         <div class="site-footer">
-             <div class="container">
-                 <div class="row mb-5">
-                     <div class="col-md-4">
-                         <h3 class="footer-heading mb-4">About Us</h3>
-                         <p>{{ Str::limit($setting->site_description, 250) }}</p>
-                     </div>
-                     <div class="col-md-3">
-                         <!-- <h3 class="footer-heading mb-4">Navigation</h3> -->
-                         <ul class="list-unstyled float-end ms-5">
-                             <li><a href="#">About Us</a></li>
-                             <li><a href="#">Advertise</a></li>
-                             <li><a href="#">Careers</a></li>
-                             <li><a href="#">Subscribes</a></li>
-                         </ul>
-                         <ul class="list-unstyled float-end">
-                             @foreach ($categories as $category)
-                             <li><a href="#">{{ Str::ucfirst($category->name) }}</a></li>
-                             @endforeach
-                         </ul>
-                     </div>
-                     <div class="col-md-4 ms-auto">
-                         <div>
-                             <h3 class="footer-heading mb-4">Connect With Us</h3>
-                             <p>
-                                 <a href="{{ $setting->site_social['facebook'] }}" target="_blank"><span class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
-                                 <a href="{{ $setting->site_social['twitter'] }}" target="_blank"><span class="icon-twitter p-2"></span></a>
-                                 <a href="{{ $setting->site_social['instagram'] }}" target="_blank"><span class="icon-instagram p-2"></span></a>
-                             </p>
-                         </div>
-                     </div>
-                 </div>
-                 <div class="row">
-                     <div class="col-12 text-center">
-                         <p>{{ $setting->site_copyright }}</p>
-                     </div>
-                 </div>
-             </div>
-         </div>
+        <!-- About Us -->
+        <div class="site-footer">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col-md-4">
+                        <h3 class="footer-heading mb-4">About Us</h3>
+                        <p>{{ Str::limit($setting->site_description, 250) }}</p>
+                    </div>
+                    <div class="col-md-3">
+                        <!-- <h3 class="footer-heading mb-4">Navigation</h3> -->
+                        <ul class="list-unstyled float-end ms-5">
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">Advertise</a></li>
+                            <li><a href="#">Careers</a></li>
+                            <li><a href="#">Subscribes</a></li>
+                        </ul>
+                        <ul class="list-unstyled float-end">
+                            @foreach ($categories as $category)
+                            <li><a href="#">{{ Str::ucfirst($category->name) }}</a></li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    <div class="col-md-4 ms-auto">
+                        <div>
+                            <h3 class="footer-heading mb-4">Connect With Us</h3>
+                            <p>
+                                <a href="{{ $setting->site_social['facebook'] }}" target="_blank"><span
+                                        class="icon-facebook pt-2 pr-2 pb-2 pl-0"></span></a>
+                                <a href="{{ $setting->site_social['twitter'] }}" target="_blank"><span
+                                        class="icon-twitter p-2"></span></a>
+                                <a href="{{ $setting->site_social['instagram'] }}" target="_blank"><span
+                                        class="icon-instagram p-2"></span></a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <p>{{ $setting->site_copyright }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-     </div>
+    </div>
 
-     <script src="{{ asset('website') }}/js/jquery-3.3.1.min.js"></script>
-     <script src="{{ asset('website') }}/js/jquery-migrate-3.0.1.min.js"></script>
-     <script src="{{ asset('website') }}/js/jquery-ui.js"></script>
-     <script src="{{ asset('website') }}/js/bootstrap.bundle.min.js"></script>
-     <script src="{{ asset('website') }}/js/owl.carousel.min.js"></script>
-     <script src="{{ asset('website') }}/js/jquery.stellar.min.js"></script>
-     <script src="{{ asset('website') }}/js/jquery.countdown.min.js"></script>
-     <script src="{{ asset('website') }}/js/jquery.magnific-popup.min.js"></script>
-     <script src="{{ asset('website') }}/js/jquery.preloader.js"></script>
-     <script src="{{ asset('website') }}/js/bootstrap-datepicker.min.js"></script>
-     <script src="{{ asset('website') }}/js/aos.js"></script>
+    <script src="{{ asset('website') }}/js/jquery-3.6.0.min.js"></script>
+    <script src="{{ asset('website') }}/js/bootstrap.bundle.min.js"></script>
 
-     <script src="{{ asset('website') }}/js/main.js"></script>
+</body>
 
-     <!-- Global site tag (gtag.js) - Google Analytics -->
-     <script async="" src="../../gtag/js.js?id=UA-23581568-13"></script>
-     <script>
-         window.dataLayer = window.dataLayer || [];
-
-         function gtag() {
-             dataLayer.push(arguments);
-         }
-         gtag('js', new Date());
-
-         gtag('config', 'UA-23581568-13');
-
-     </script>
-
- </body>
-
- </html>
+</html>
