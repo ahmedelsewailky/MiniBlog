4e17a8ea97776e6aa4afb323fa13f9c305a9e10a
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-08-29 13:39:20 +0200

    Handling path to admin dashboard

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -11,13 +11,13 @@ use Illuminate\Support\Facades\Route;
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * The path to the "home" route for your application.
+     * The path to the "admin" route for your application.
      *
      * This is used by Laravel authentication to redirect users after login.
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/panel';
 
     /**
      * The controller namespace for the application.
@@ -46,6 +46,10 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
+
+            Route::middleware('dashboard')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/dashboard.php'));
         });
     }
 
