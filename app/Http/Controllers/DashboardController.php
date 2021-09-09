a57013bbf288417078c787cd01a1bcb952c6ebf2
Ahmed Elsewailky <75929591+ahmedelsewailky@users.noreply.github.com>
2021-09-09 14:56:21 +0200

    Add chart pies

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,8 @@ use App\Models\Post;
 use App\Models\User;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
@@ -29,6 +31,17 @@ class DashboardController extends Controller
         $users = User::take(3)->get();
         $posts = Post::latest()->get();
         $setting = Setting::all()->first();
-        return view('dashboard.index', compact(['users', 'posts', 'setting']));
+        $record = Post::select(DB::raw("COUNT(*) as count"), DB::raw("DAYNAME(created_at) as day_name"), DB::raw("DAY(created_at) as day"))
+        ->where('created_at', '>', Carbon::today()->subDay(6))
+        ->groupBy('day_name', 'day')
+        ->orderBy('day')
+        ->get();
+        $data = [];
+        foreach ($record as $row) {
+            $data['label'][] = $row->day_name;
+            $data['data'][] = (int) $row->count;
+        }
+        $data['chart_data'] = json_encode($data);
+        return view('dashboard.index', compact(['users', 'posts', 'setting', 'data']));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 @section('title', 'Dashboard')
 @section('breadcrumbs')
-    <li class="breadcrumb-item active" aria-current="page"><i class="fas fa-home"></i> Dashboard</li>
+<li class="breadcrumb-item active" aria-current="page"><i class="fas fa-home"></i> Dashboard</li>
 @endsection
 @section('content')
 <!-- Website Overview -->
@@ -38,6 +38,18 @@
     </div>
 </div>
 
+<!-- Charts -->
+<div class="card mb-3">
+    <div class="card-header  main-color-bg">
+        Charts
+    </div>
+    <div class="card-body">
+        <div class="pie-chart-container">
+            <canvas id="pie-chart"></canvas>
+        </div>
+    </div>
+</div>
+
 <!-- Latest Users -->
 <div class="card">
     <div class="card-header  main-color-bg">
@@ -60,4 +72,71 @@
         </table>
     </div>
 </div>
+@endsection
+@section('custom-script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.js"></script>
+<script>
+    $(function () {
+        //get the pie chart canvas
+        var cData = JSON.parse(`<?php echo $data['chart_data']; ?>`);
+        var ctx = $("#pie-chart");
+
+        //pie chart data
+        var data = {
+            labels: cData.label,
+            datasets: [{
+                label: "Users Count",
+                data: cData.data,
+                backgroundColor: [
+                    "#DEB887",
+                    "#A9A9A9",
+                    "#DC143C",
+                    "#F4A460",
+                    "#2E8B57",
+                    "#1D7A46",
+                    "#CDA776",
+                ],
+                borderColor: [
+                    "#CDA776",
+                    "#989898",
+                    "#CB252B",
+                    "#E39371",
+                    "#1D7A46",
+                    "#F4A460",
+                    "#CDA776",
+                ],
+                borderWidth: [1, 1, 1, 1, 1, 1, 1]
+            }]
+        };
+
+        //options
+        var options = {
+            responsive: true,
+            title: {
+                display: true,
+                position: "top",
+                text: "Last week new post -  Day wise count",
+                fontSize: 18,
+                fontColor: "#111"
+            },
+            legend: {
+                display: true,
+                position: "bottom",
+                labels: {
+                    fontColor: "#333",
+                    fontSize: 16
+                }
+            }
+        };
+
+        //create Pie Chart class object
+        var chart1 = new Chart(ctx, {
+            type: "pie",
+            data: data,
+            options: options
+        });
+
+    });
+
+</script>
 @endsection
